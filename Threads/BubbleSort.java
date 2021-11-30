55ab7557067e0b79b4e38edc0d95fdc5e4519383
Ril <rilwanhuzain@gmail.com>
2021-11-30 11:01:34 +0530

    Files added..

--- Threads/BubbleSort.java
+++ Threads/BubbleSort.java
@@ -1,32 +1,51 @@
 //  3. Try enable and disable JIT and checkout the results (Write a bubble sort program to sort 10000 numbers with and without JIT!)
 
 import java.util.Random;
+import java.io.File;
+import java.util.Scanner;
 
-public class BubbleSort
-{
-    public static void main(String[] args)
+public class BubbleSort {
+
+    public static void main(String[] args) throws Exception 
     {
+        long startTime = System.nanoTime();
 
-        Random rgen = new Random();  // Random number generator
-        int[] nums = new int[10000];  //array to store 10000 random integers (1-10,000)
+        // Read the text file containing the 10,000 random numbers..
+        File file = new File("10000 random numbers.txt");
+        Scanner input = new Scanner(file);
 
-        //--- Initialize the array to the ints 1-10,000
-        for (int i=0; i<nums.length; i++) {
-            nums[i] = i;
+        String line = input.nextLine();
+        String[] numbers = line.split(" ");
+        int[] arr = new int[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            arr[i] = Integer.parseInt(numbers[i]);
         }
 
-        //--- Shuffle by exchanging each element randomly
-        for (int i=0; i<nums.length; i++) {
-            int randomPosition = rgen.nextInt(nums.length);
-            int temp = nums[i];
-            nums[i] = nums[randomPosition];
-            nums[randomPosition] = temp;
+        int n = arr.length;
+        int temp = 0;
+        for(int i = 0;i<n;i++) 
+        {
+            for(int j = 1;j<(n-i);j++) 
+            {
+                if(arr[j-1] > arr[j]) 
+                {
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
 
-        //Print results
-        for (int i = 0; i < nums.length; i++){
-        System.out.println(nums[i]);
+        for (int i = 0; i < n; i++) 
+        {
+            System.out.print(arr[i] + " ");
+        }
         System.out.println("\n");
+
+        // print the time taken for the program...
+        long endTime = System.nanoTime();
+        System.out.println("Time taken :" + (endTime - startTime) + " nano-seconds");
     }
-}
 }
